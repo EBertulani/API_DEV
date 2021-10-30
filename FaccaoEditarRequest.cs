ecf0e65405a9dc4f79d44f618f88f13fb2565d14
EBertulani <bertulani.ewerton@gmail.com>
2021-10-30 11:29:18 -0300

    Add files via upload

---
+++ FaccaoEditarRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.DataTransfer.Faccoes.Requests
+{
+    public class FaccaoEditarRequest
+    {
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+        public string UFOrigem { get; set; }
+        public string AnoOrigem { get; set; }
+        public string NomeLider { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DataDesativacao { get; set; }
+        public string MotivoDesativacao { get; set; }
+    }
+}
